5faab83111b2dc2d77936c6445137e92cb8f4d5f
Fredrik <valdt@kth.se>
2015-11-02 16:02:04 +0100

    lol

--- client/client.py
+++ client/client.py
@@ -4,12 +4,8 @@ from clientClasses import *
 
 def listenIncoming(clientSocket, packetHandler):
     if clientSocket:
-        print("socket")
         try:
             data = pickle.loads(clientSocket.recv(10000))
-            print("data tried and succeeded")
-            x = ["handshake","McFritte"]
-            clientSocket.send(pickle.dumps(x))
             if data != "":
                 print(data)
                 if data[0] == "dungeonUpdate":
@@ -17,8 +13,14 @@ def listenIncoming(clientSocket, packetHandler):
 
                 elif data[0] == "wumpusUpdate":
                     packetHandler.wu.update(data)
-        except Exception:
-            print("exception")
+
+                elif data[0] == "handshake":
+                    x = ["handshake","McFritte"]
+                    clientSocket.send(pickle.dumps(x))
+                    print("Handshake recieved and response sent!")
+
+        except:
+            pass
 
 def initSocket(ip, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,14 +28,14 @@ def initSocket(ip, port):
     return s
 
 def main():
-    clientSocket = initSocket('192.168.1.108', 1337)
+    clientSocket = initSocket('localhost', 1337)
     packetHandler = PacketHandler()
     print("initiated")
-        listenThread = threading.Thread(target=listenIncoming, args = (clientSocket, packetHandler))
-        listenThread.start()
+    listenThread = threading.Thread(target=listenIncoming, args=(clientSocket, packetHandler))
+    listenThread.start()
 
     while True:
-        #print(clientSocket.recv(10000))
-        listenThread.run()
-        
+        if not listenThread.isAlive:
+            listenThread.run()
+
 main()
